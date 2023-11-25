efe801642ff8961683a8ed83e306c605a16a77c6
the-coder-boy <shreejanOfficial2007@gmail.com>
2023-11-25 13:29:26 +0530

    Weather with image

--- api/index.py
+++ api/index.py
@@ -2,8 +2,12 @@ from flask import Flask, render_template
 app = Flask(__name__, static_url_path='/static')
 
 @app.route('/')
-def hello_world():
+def index():
     return render_template('index.html')
+
+@app.route('/weather')
+def weather():
+    return render_template('weather.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
--- api/static/js/index.js
+++ api/static/js/index.js
@@ -1,15 +1,41 @@
-window.addEventListener("scroll", function(){
-    const header = this.document.querySelector("header")
-    header.classList.toggle("sticky", window.scrollY>0);
-})
+window.addEventListener("scroll", function () {
+  const header = document.querySelector("header");
+  header.classList.toggle("sticky", window.scrollY > 0);
+});
 
 const menuBtn = document.querySelector(".nav-menu-btn");
 const closeBtn = document.querySelector(".nav-close-btn");
 const navigation = document.querySelector(".navigation");
 
 menuBtn.addEventListener("click", () => {
-    navigation.classList.add("active");
+  navigation.classList.toggle("active");
 });
+
 closeBtn.addEventListener("click", () => {
+  navigation.classList.toggle("active");
+});
+
+// Close the navigation menu if a navigation link is clicked
+const navLinks = document.querySelectorAll(".nav-items a");
+navLinks.forEach((link) => {
+  link.addEventListener("click", () => {
     navigation.classList.remove("active");
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const imageBox = document.querySelector(".image-box");
+
+  // Function to fetch a random nature image from Unsplash
+  function getRandomNatureImage() {
+    const randomParam = Math.random(1); 
+    const imageUrl = `https://source.unsplash.com/featured/?nature&${randomParam}`;
+
+    // Create an image element and append it to the image box
+    const imageElement = document.createElement("img");
+    imageElement.src = imageUrl;
+    imageBox.appendChild(imageElement);
+  }
+
+  getRandomNatureImage();
 });
